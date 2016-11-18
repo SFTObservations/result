f0c9525c9417be354eef204fb63e61ba3afe168a
SFTObservations <bhanuprakasha.hb@mphasis.com>
2016-11-18 20:09:33 +0530

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,263 @@
+var data = [  
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1373311' target='_blank'>SIO1373311</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Sublist's under Sound, Video and game controller in Device security is not localized in Finnish",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3 ",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"16-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1373310' target='_blank'>SIO1373310</a>",
+      "DESCRIPTION":"SFT-security-Reskin: Text not localized under Audio input and outputs in Device Security for Finnish",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3 ",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"16-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1373309' target='_blank'>SIO1373309</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Under software devices in Device security it's not localized to Finnish",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"16-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1373303' target='_blank'>SIO1373303</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Biometric devices hardware is not localized in Finnish",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"16-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1373285' target='_blank'>SIO1373285</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Administrators is not localized in pop for Yes/No window for Finish",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"16-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1373276' target='_blank'>SIO1373276</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Network adapters not showing in local language for Finnish under Device Security",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"16-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1373135' target='_blank'>SIO1373135</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Sublist under Smart cards from Device security tab is not Localized in Finnish",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"16-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1373112' target='_blank'>SIO1373112</a>",
+      "DESCRIPTION":"SFT-Security Reskin: Devices listed under Software devices tab are coming in english for Finnish language",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"16-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372850' target='_blank'>SIO1372850</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Text overlap with the down arrow",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372861' target='_blank'>SIO1372861</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Device Access Manager with Just In Time authentication is coming in English for Greek ",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372871' target='_blank'>SIO1372871</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Under Password Manager for Greek language string ",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372884' target='_blank'>SIO1372884</a>",
+      "DESCRIPTION":"SFT- Security -Reskin: In Device Security, under Sound, Video and game controller in Administrator the tab is chopped before Allow tab",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372886' target='_blank'>SIO1372886</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Device Security/Hardware/ Sound, Video and game controller is coming in English for Dutch",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372889' target='_blank'>SIO1372889</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Approval button is having english 'OK' for Dutch language",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372893' target='_blank'>SIO1372893</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: At the first page when HP CSM is launched the lower part of the Device security logo and it's title is chopped for Dutch language",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372896' target='_blank'>SIO1372896</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Right side part of the Device access manager page is chopped of when minimized for Polish",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372899' target='_blank'>SIO1372899</a>",
+      "DESCRIPTION":"SFT- Security-reskin: Enable/Disable button is chopped at Device Security page for 'Device Access Manager with Just in Time authentication' table",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372900' target='_blank'>SIO1372900</a>",
+      "DESCRIPTION":"SFT-Security-Reskin: Standard user policies tab is chopped in the right side of first page of HP Client security manager in Swedish",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Serious",
+      "CREATE DATE":"15-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372551' target='_blank'>SIO1372551</a>",
+      "DESCRIPTION":"SFT-Security-Reskin : In Dashboard, under Identity Security, In many sections ",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372549' target='_blank'>SIO1372549</a>",
+      "DESCRIPTION":"SFT-Security-Reskin [Local] : In Dashboard, under Bluetooth in settings tab, the checkbox statement under modify bluetooth settings is not localized for DA.",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372548' target='_blank'>SIO1372548</a>",
+      "DESCRIPTION":"SFT-Security-Reskin [Local] : ",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372547' target='_blank'>SIO1372547</a>",
+      "DESCRIPTION":"SFT-Security-Reskin [Local] : The Dash board title is not localized for DA and NL",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372546' target='_blank'>SIO1372546</a>",
+      "DESCRIPTION":"SFT-Security-Reskin [Local] : Incorrect Password warning message is not Localized on Administrators Policies, Standard User Policies, Security Features on AR localized system.",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372545' target='_blank'>SIO1372545</a>",
+      "DESCRIPTION":"SFT-Security-Reskin [Local] : Non-localized entities seen on Identity Security Screen of TR, DA, FI, NL, SV and PL",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372544' target='_blank'>SIO1372544</a>",
+      "DESCRIPTION":"SFT-Security-Reskin [Local] : There is cut-off in HP Sparekey Page in Danish, Dutch and Swedish languages. Seen during the resizing of window.",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372543' target='_blank'>SIO1372543</a>",
+      "DESCRIPTION":"SFT-Security-Reskin [Local] : Warning message for wrong windows password is not shown in local language in Arabic",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372542' target='_blank'>SIO1372542</a>",
+      "DESCRIPTION":"SFT-Security-Reskin : Two shortcuts of HP CSM is created in the startup menu after installing",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372540' target='_blank'>SIO1372540</a>",
+      "DESCRIPTION":" SFT-Security-Reskin [Local] : Welcome note is not in bold letters in the Install Wizard screen in Turkish",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   },
+   {  
+      "CORE TEAM":"Security",
+      "OBS NUMBER":"<a href='https://si.austin.hp.com/si/Observations/Details.aspx?slim=yes&offset=5.5&ObservationId=SIO1372533' target='_blank'>SIO1372533</a>",
+      "DESCRIPTION":"SFT-Security-Reskin [Local] : Instead of HP Client Security Manager - InstallShield Wizard it shows InstallShield Wizard for HP Client Security Manager in Welcome \nscreen of Installer in Swedish language",
+      "COMPONENT":"HP Client Security Manager-9.2.0.2293.A.3",
+      "CURRENT STATE":"Open",
+      "PRIORITY":"Medium",
+      "CREATE DATE":"14-Nov-16"
+   }
+];
